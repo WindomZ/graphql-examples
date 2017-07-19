ffe835edfab79ff531d186f7c3cc1d074b8b2b46
WindomZ <git.windomz@gmail.com>
2017-07-19 15:14:05 +0800

    feat(php): init php example

---
+++ php/serve.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * User: WindomZ
+ * Date: 17-7-19
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Schema;
+use GraphQL\GraphQL;
+
+try {
+    $queryType = new ObjectType([
+        'name' => 'Query',
+        'fields' => [
+            'echo' => [
+                'type' => Type::string(),
+                'args' => [
+                    'message' => ['type' => Type::string()],
+                ],
+                'resolve' => function ($root, $args) {
+                    return $root['prefix'].$args['message'];
+                }
+            ],
+        ],
+    ]);
+
+    $mutationType = new ObjectType([
+        'name' => 'Calc',
+        'fields' => [
+            'sum' => [
+                'type' => Type::int(),
+                'args' => [
+                    'x' => ['type' => Type::int()],
+                    'y' => ['type' => Type::int()],
+                ],
+                'resolve' => function ($root, $args) {
+                    return $args['x'] + $args['y'];
+                },
+            ],
+        ],
+    ]);
+
+    $schema = new Schema([
+        'query' => $queryType,
+        'mutation' => $mutationType,
+    ]);
+
+    $rawInput = file_get_contents('php://input');
+    $input = json_decode($rawInput, true);
+    $query = $input['query'];
+    $variableValues = isset($input['variables']) ? $input['variables'] : null;
+
+    $rootValue = ['prefix' => 'You said: '];
+    $result = GraphQL::execute($schema, $query, $rootValue, null, $variableValues);
+} catch (\Exception $e) {
+    $result = [
+        'error' => [
+            'message' => $e->getMessage()
+        ]
+    ];
+}
+header('Content-Type: application/json; charset=UTF-8');
+echo json_encode($result);
