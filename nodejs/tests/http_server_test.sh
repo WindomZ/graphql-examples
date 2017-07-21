b8a007868e71b7e7a5af20d286d441fb19b45faa
WindomZ <git.windomz@gmail.com>
2017-07-21 16:30:31 +0800

    test(nodejs): add shell testing

--- nodejs/tests/http_server_test.sh
+++ nodejs/tests/http_server_test.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
-curl -X POST \
+resp=$(curl -q -s -X POST \
   -H "Content-Type: application/json" \
   -d '{"query": "query { hello(message: \"world\") }"}' \
-  http://localhost:8080/graphql
+  http://localhost:8080/graphql)
+[[ "$resp" == "{\"data\":{\"hello\":\"Hello world!\"}}" ]] \
+  && result="\033[32mYES\033[0m" || result="\033[31mNO\033[0m"
+echo -e "$resp $result"
 
-curl -X POST \
+resp=$(curl -q -s -X POST \
   -H "Content-Type: application/json" \
   -d '{"query": "query { bye }"}' \
-  http://localhost:8080/graphql
+  http://localhost:8080/graphql)
+[[ "$resp" == "{\"data\":{\"bye\":\"Goodbye!\"}}" ]] \
+  && result="\033[32mYES\033[0m" || result="\033[31mNO\033[0m"
+echo -e "$resp $result"
 
-curl -X POST \
+resp=$(curl -q -s -X POST \
   -H "Content-Type: application/json" \
   -d '{"query": "mutation { sum(x: 1, y: 2) }" }' \
-  http://localhost:8080/graphql
+  http://localhost:8080/graphql)
+[[ "$resp" == "{\"data\":{\"sum\":3}}" ]] \
+  && result="\033[32mYES\033[0m" || result="\033[31mNO\033[0m"
+echo -e "$resp $result"
